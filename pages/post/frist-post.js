a22c981dc46f7e61f683c51b30222d957e1afe5b
soshiharami <soshi0807sss@gmail.com>
2020-12-25 15:59:49 +0900

    Head title

--- pages/post/frist-post.js
+++ pages/post/frist-post.js
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import Head from 'next/head'
+
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>First Post</title>
+            </Head>
             <h1>This is first Page</h1>
             <h2>
                 <Link href={"/"}>
