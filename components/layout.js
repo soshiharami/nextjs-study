6ef436c12a86a72b9dffb9e63822fdb155647ccc
soshiharami <soshi0807sss@gmail.com>
2020-12-25 17:09:39 +0900

    layout

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,67 @@
 import styles from '../styles/layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Head from 'next/head'
+import home from "../pages/index"
 
-function Layout({ children }) {
+const name = 'soshi'
+export const siteTirle = 'Next.js!!!!!'
+
+function Layout({children}) {
     return (
         <div className={styles.container}>
-            {children}
+            <Head>
+                <link rel={"icon"} href={"/favicon.ico"}/>
+                <meta
+                    name={"description"}
+                    content={"れんちょんはかわいいです。"}
+                />
+                <meta
+                    property="og:image"
+                    content={`https://og-image.now.sh/${encodeURI(
+                        siteTirle
+                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+                <meta name="og:title" content={siteTirle}/>
+                <meta name="twitter:card" content="summary_large_image"/>
+            </Head>
+            <header className={styles.header}>
+                {home ? (
+                    <>
+                        <img
+                            src="/images/renryon.jpg"
+                            className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+                            alt={name}
+                        />
+                        <h1 className={utilStyles.heading2Xl}>{name}</h1>
+                    </>
+                ) : (
+                    <>
+                        <Link href="/">
+                            <a>
+                                <img
+                                    src="/public/images/renryon.jpg"
+                                    className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+                                    alt={name}
+                                />
+                            </a>
+                        </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>{name}</a>
+                            </Link>
+                        </h2>
+                    </>
+                )}
+            </header>
+            <main>{children}</main>
+            {!home && (
+                <div className={styles.backToHome}>
+                    <Link href="/">
+                        <a>← Back to home</a>
+                    </Link>
+                </div>
+            )}
         </div>
     )
 }
